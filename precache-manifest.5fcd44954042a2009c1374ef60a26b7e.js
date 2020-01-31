9cb20d5bb59d1d6a32ff98e67140ff00edbbf5b2
Reese <Bensonre@oregonstate.edu>
2020-01-30 23:24:09 -0800

    Updates

---
+++ precache-manifest.5fcd44954042a2009c1374ef60a26b7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cf06f588ed3f21ce6a92d7f751f38c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "eebeb2265902d89da947",
+    "url": "/my-app/static/css/main.17d1e838.chunk.css"
+  },
+  {
+    "revision": "409959f00e24cbae5072",
+    "url": "/my-app/static/js/2.7a204919.chunk.js"
+  },
+  {
+    "revision": "eebeb2265902d89da947",
+    "url": "/my-app/static/js/main.3a6cfa2b.chunk.js"
+  },
+  {
+    "revision": "9c47355d93f1cffa1d27",
+    "url": "/my-app/static/js/runtime~main.eed5ce82.js"
+  },
+  {
+    "revision": "71a9e20f6e21e847618e4e5eaaf9c658",
+    "url": "/my-app/static/media/MeLogo.71a9e20f.svg"
+  },
+  {
+    "revision": "26a2d7878de15f616a53bb172eff0da8",
+    "url": "/my-app/static/media/banner.26a2d787.png"
+  }
+]);
